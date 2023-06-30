4e7941fc4364884699b4f3d8047ca439b1a005aa
Армен Погосян <armen-pogosyan@mail.ru>
2023-07-01 01:04:44 +0300

    ПР15_1.67

--- backend/middlewares/cors.js
+++ backend/middlewares/cors.js
@@ -12,6 +12,7 @@ module.exports = (req, res, next) => {
 
   const { method } = req; // Сохраняем тип запроса (HTTP-метод) в соответствующую переменную
 
+  const requestHeaders = req.headers['access-control-request-headers'];
   // Значение для заголовка Access-Control-Allow-Methods по умолчанию (разрешены все типы запросов)
   const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
 
@@ -19,6 +20,8 @@ module.exports = (req, res, next) => {
   if (method === 'OPTIONS') {
     // разрешаем кросс-доменные запросы любых типов (по умолчанию)
     res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    // завершаем обработку запроса и возвращаем результат клиенту
     return res.end();
   }
 
