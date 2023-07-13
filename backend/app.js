b0cbe88ef13ee19a3891d6372102f5ed9229da5c
Армен Погосян <armen-pogosyan@mail.ru>
2023-07-13 20:06:48 +0300

    Бэкед 1.3

--- backend/app.js
+++ backend/app.js
@@ -25,11 +25,11 @@ app.post('/signin', celebrate({
     password: Joi.string().required().min(8),
   }),
 }), login);
-app.get('/crash-test', () => {
-  setTimeout(() => {
-    throw new Error('Сервер сейчас упадёт');
-  }, 0);
-});
+// app.get('/crash-test', () => {
+//   setTimeout(() => {
+//     throw new Error('Сервер сейчас упадёт');
+//   }, 0);
+// });
 app.post('/signup', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
